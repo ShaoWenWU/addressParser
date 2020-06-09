221a5e1d7325adb36a68567f253915f62823f99e
Shao-Wen Wu <wu8149@gmail.com>
2020-06-09 18:02:28 +0200

    coding style conformance test

--- addressParser.py
+++ addressParser.py
@@ -1,44 +1,65 @@
-print("Starting address parser...")
 from postal.parser import parse_address
 import json
 import re
 from collections import OrderedDict
 
+
 def addressParser(inputAddress):
-	
-			parsedAddress = parse_address(inputAddress)
-			try:
-				try:
-					parsedHouseNumber = [element for element in parsedAddress if 'house_number' in element[1]][0][0]
-				except IndexError:
-					parsedHouseNumber = ''
 
-				houseNumber = re.search(parsedHouseNumber, inputAddress, flags=re.IGNORECASE).group(0)
+    parsedAddress = parse_address(inputAddress)
+    print(parsedAddress)
+    try:
+        try:
+            # extract the first house_number value
+            parsedHouseNumber = [element for element in parsedAddress
+                                 if 'house_number' in element[1]][0][0]
 
-				streetArray = [element for element in parsedAddress if 'house_number' not in element[1]]
+        except IndexError:
+            parsedHouseNumber = ''
 
-				parsedStreet = ' '.join([str(x[0]) for x in streetArray]) 
+        houseNumber = re.search(
+            parsedHouseNumber, inputAddress, flags=re.IGNORECASE).group(0)
 
-				# substitute houseNumber with empty in the input string, 
-				# igore leading and trailing space and specific chracter
-				street = re.sub(houseNumber,'',inputAddress, flags=re.IGNORECASE ).rstrip('}{[]()?@$%^*<>/\\\"\'~;:-_, ').lstrip('}{[]()?@$%^*<>/\\\"\'~;:-_,. ').replace('  ', ' ').replace(' ,', ',')
+        streetArray = [element for element in parsedAddress
+                       if 'house_number' not in element[1]]
 
-				addressDict = OrderedDict( [("street",street), ("housenumber",houseNumber)] )
-				return json.dumps(addressDict, ensure_ascii=False) # retnrn json object with orderedDict
-			except Exception as e:
-				print(e)
+        parsedStreet = ' '.join([str(x[0]) for x in streetArray])
+
+        # substitute houseNumber with empty in the input string,
+        # igore leading and trailing space and specific chracter
+        street = re.sub(
+                        houseNumber, '',
+                        inputAddress,
+                        flags=re.IGNORECASE
+                        ).rstrip(
+                                    '}{[]()?@$%^*<>/\\\"\'~;:-_, '
+                        ).lstrip(
+                                    '}{[]()?@$%^*<>/\\\"\'~;:-_,. '
+                        ).replace(
+                                    '  ', ' '
+                        ).replace(
+                                    ' ,', ','
+                        )
+
+        addressDict = OrderedDict([("street", street),
+                                  ("housenumber", houseNumber)])
+        # retnrn json object with orderedDict
+        return json.dumps(addressDict, ensure_ascii=False)
+    except Exception as e:
+        print(e)
 
 
 def main():
-	print("Enter \"exit()\" to exit the script.")
+    print("Enter \"exit()\" to exit the script.")
 
-	while(1):
-		inputAddress =  input("\nEnter one-line address: \n")
+    while(1):
+        inputAddress = input("\nEnter one-line address: \n")
 
-		if(inputAddress == "exit()"):
-			break;
-		else:
-			print(addressParser(inputAddress))
+        if(inputAddress == "exit()"):
+            break
+        else:
+            print(addressParser(inputAddress))
+
 
 if __name__ == "__main__":
     main()
--- test_addressParser.py
+++ test_addressParser.py
@@ -1,41 +1,50 @@
 import unittest
 import addressParser
 import csv
-from ddt import ddt,data,unpack
+from ddt import ddt, data, unpack
 import HtmlTestRunner
+import pycodestyle
+
 
 def importTestCase(file_name):
-	rows=[]
-	with open(file_name,encoding='utf-8-sig') as f:
-		readers = csv.DictReader(f, delimiter=',')
-		for row in readers:
-			rows.append([row['Input'], row['Expected']])
-	print("Imported DATA: ", file_name, "\n")
-	return rows
+    rows = []
+    with open(file_name, encoding='utf-8-sig') as f:
+        readers = csv.DictReader(f, delimiter=',')
+        for row in readers:
+            rows.append([row['Input'], row['Expected']])
+    print("Imported DATA: ", file_name, "\n")
+    return rows
 
 
 @ddt
 class Test_addressParser(unittest.TestCase):
 
-	def setUp(self):
-		pass
-	
-	
-	@data(*importTestCase('testCase.csv'))
-	@unpack
-	def test_addressParser(self, inputAddress, expected):
-		result = addressParser.addressParser(inputAddress)
-		self.assertEqual(result, expected)
+    def setUp(self):
+        # self.path = ['.', ]  # Check all file within current project
+        self.path = ['addressParser.py', 'test_addressParser.py']
+        # pass
 
+    def test_conformance(self):
+        """Test that we conform to PEP-8."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(self.path)
+        self.assertEqual(
+            result.total_errors, 0, "Found code style errors (and warnings).")
+
+    @data(*importTestCase('testCase.csv'))
+    @unpack
+    def test_addressParser(self, inputAddress, expected):
+        result = addressParser.addressParser(inputAddress)
+        self.assertEqual(result, expected)
 
 
 if __name__ == '__main__':
-	suite = unittest.TestLoader().loadTestsFromTestCase(Test_addressParser)
+    suite = unittest.TestLoader().loadTestsFromTestCase(Test_addressParser)
 
-	runner = HtmlTestRunner.HTMLTestRunner(
-				verbosity=1,
-				report_title='Test Report',
-				descriptions='Test report of multiple input cases.'
+    runner = HtmlTestRunner.HTMLTestRunner(
+                verbosity=1,
+                report_title='Test Report',
+                descriptions='Test report of multiple input cases.'
                 )
 
-	runner.run(suite)
+    runner.run(suite)
